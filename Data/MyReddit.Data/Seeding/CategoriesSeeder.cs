f0780ba5a15979c54ff3505088e1a8d337f89a64
Anzzhhela98 <anzzhhela98@gmail.com>
2023-09-03 22:06:11 +0300

    CategoriesSeeder

---
+++ Data/MyReddit.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,32 @@
+﻿namespace MyReddit.Data.Seeding
+{
+    using MyReddit.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Categories.Any())
+            {
+                return;
+            }
+
+            var categories = new List<string> { "Beauty","Sport","Health","Food" };
+
+            foreach (var category in categories)
+            {
+                dbContext.Categories.Add(new Category
+                {
+                    Name = category,
+                    Title = category,
+                    Description = category,
+                    ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfCZbQxq2JneYZetHTNrF--jc3sriaUco-2uM3JD17PA&s",
+                });
+            }
+        }
+    }
+}
